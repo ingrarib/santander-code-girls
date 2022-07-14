7c44e11e6ba119537cf474d1264d9cf9d8e92554
ingrarib <ircm.1998@gmail.com>
2022-07-14 14:43:24 -0300

    Adiciona exercícios de Sobrecarga

---
+++ Começando a Programar em Java/2 - Entendendo Métodos Java/Exercícios Sobrecarga/src/dio/basecamp/Main.java
@@ -0,0 +1,18 @@
+package dio.basecamp;
+
+// Exemplo para o exercício da Aula 2 de Métodos, Sobrecarga.
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+    }
+
+}
