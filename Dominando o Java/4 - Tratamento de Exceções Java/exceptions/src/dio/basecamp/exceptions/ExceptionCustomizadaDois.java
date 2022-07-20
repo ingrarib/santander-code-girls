6e115e10c8d825bdb0b31b85245530dae885da21
ingrarib <ircm.1998@gmail.com>
2022-07-20 12:22:51 -0300

    Adicionar exercícios sobre Exceptions

---
+++ Dominando o Java/4 - Tratamento de Exceções Java/exceptions/src/dio/basecamp/exceptions/ExceptionCustomizadaDois.java
@@ -0,0 +1,26 @@
+package dio.basecamp.exceptions;
+
+
+import javax.swing.*;
+
+public class ExceptionCustomizadaDois {
+    public static void main(String[] args){
+        int[] numerador = {4, 5, 8, 10};
+        int[] denominador = {2, 4, 0, 2, 8};
+
+        for (int i = 0; i < denominador.length; i++) {
+            try {
+                if(numerador[i] %2 != 0)
+                    throw new DivisaoNaoExataException("Divisão não exata!", numerador[i], denominador[i]);
+
+                int resultado = numerador[i] / denominador[i];
+                System.out.println(resultado);
+            } catch (DivisaoNaoExataException | ArithmeticException | ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+        }
+
+        System.out.println("O programa continua...");
+    }
+}
